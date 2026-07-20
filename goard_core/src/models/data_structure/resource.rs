73dd5dd7a2cbe89aca10c819ce85284f4e8db8bf
erramim <Moataz.Er-Rami@etu.univ-grenoble-alpes.fr>
2026-07-20 13:53:06 +0200

    [UPDATE] added dead ressources to web version

--- goard_core/src/models/data_structure/resource.rs
+++ goard_core/src/models/data_structure/resource.rs
@@ -36,7 +36,7 @@ impl Display for ResourceState {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
 pub struct DeadInterval {
     pub start_s: i64,
     pub end_s: i64,
